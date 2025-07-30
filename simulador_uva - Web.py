e22e785508c368b0b1389101ec35db2411291dff
NahuelZanzi <89280851+NahuelZanzi@users.noreply.github.com>
2025-07-30 17:24:29 -0300

    Update simulador_uva - Web.py

--- simulador_uva - Web.py
+++ simulador_uva - Web.py
@@ -20,8 +20,8 @@ valor_uva = st.cache_data(ttl=60*60*20)(lambda: int(float(next(tr.find_all('td')
 st.set_page_config(page_title="Simulador Crédito UVA", layout="centered")
 
 #dolares
-oficial = st.cache_data(ttl=60)(lambda: requests.get("https://dolarapi.com/v1/dolares/oficial").json())()
-blue = st.cache_data(ttl=60)(lambda: requests.get("https://dolarapi.com/v1/dolares/blue").json())()
+oficial = st.cache_data(ttl=60*60*5)(lambda: requests.get("https://dolarapi.com/v1/dolares/oficial").json())()
+blue = st.cache_data(ttl=60*60*5)(lambda: requests.get("https://dolarapi.com/v1/dolares/blue").json())()
 uva = valor_uva
 st.markdown(f"""
 <style>
