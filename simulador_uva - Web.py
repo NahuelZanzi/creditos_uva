279b97f5816a7cd37dac7382ebc6106d7f651691
NahuelZanzi <89280851+NahuelZanzi@users.noreply.github.com>
2025-07-30 16:54:06 -0300

    Update simulador_uva - Web.py

--- simulador_uva - Web.py
+++ simulador_uva - Web.py
@@ -77,7 +77,7 @@ st.markdown(f"""
 # Tarjeta LinkedIn separada estilo dólar (más arriba que la anterior)
 st.markdown(
     """
-    <div style="position: fixed; left: 20px; top: 220px; background: white;
+    <div style="position: fixed; left: 20px; top: 400px; background: white;
                 padding: 15px; border-radius: 10px; box-shadow: 0 2px 10px rgba(0,0,0,0.1);
                 width: 250px; z-index: 999; font-size: 16px; font-weight: bold; color: #222222;">
         👤 Created by <a href="https://www.linkedin.com/in/nahuel-martin/" target="_blank">Nahuel Martín</a>
